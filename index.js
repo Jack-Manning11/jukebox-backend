4fb514d3aee714ce8bddb973841d92e8d69801b6
Jack-Manning11 <jama0209@colorado.edu>
2023-12-08 10:15:04 -0700

    undo dumb

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-// Handle preflight requests for the /login route
-app.options('/login', cors());
-
-app.post('/login', cors(), async (req, res) => {
+app.post("/login", async (req, res) => {
     const { code } = req.body;
     const spotifyApi = new SpotifyWebApi({
         redirectUri: process.env.REDIRECT_URI,
@@ -28,7 +25,6 @@ app.post('/login', cors(), async (req, res) => {
             body: { access_token, refresh_token, expires_in },
         } = await spotifyApi.authorizationCodeGrant(code);
 
-        res.header('Access-Control-Allow-Origin', '*'); // Set the allowed origin
         res.json({ access_token, refresh_token, expires_in });
     } catch (err) {
         console.log(err);
@@ -36,10 +32,7 @@ app.post('/login', cors(), async (req, res) => {
     }
 });
 
-// Handle preflight requests for the /refresh route
-app.options('/refresh', cors());
-
-app.post('/refresh', cors(), async (req, res) => {
+app.post("/refresh", async (req, res) => {
     const { refreshToken } = req.body;
     const spotifyApi = new SpotifyWebApi({
         redirectUri: process.env.REDIRECT_URI,
@@ -52,8 +45,6 @@ app.post('/refresh', cors(), async (req, res) => {
         const {
             body : { access_token, expires_in },
         } = await spotifyApi.refreshAccessToken();
-
-        res.header('Access-Control-Allow-Origin', '*'); // Set the allowed origin
         res.json({ access_token, expires_in });
     } catch(err) {
         console.log(err);
@@ -61,9 +52,9 @@ app.post('/refresh', cors(), async (req, res) => {
     }
 });
 
-app.listen(PORT, "0.0.0.0", err => {
+app.listen(port, "0.0.0.0", err => {
     if(err) {
         console.log(err);
     }
-    console.log("Listening on port: ", PORT);
+    console.log("Listening on port: ", port);
 })
